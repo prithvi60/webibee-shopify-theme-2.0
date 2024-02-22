b08e7ab40ec443bd58ab3567af42f7c541bd2451
unknown <gokulgandhi97@gmail.com>
2024-02-22 09:38:04 +0530

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+  ],
+  // content: ['./**/*.{liquid,js,json}'],
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '750px',
+      lg: '990px',
+      xl: '1440px',
+      // x2lg: '1920px',
+      // pageMaxWidth: '1440px',
+    },
+    extend: {
+      colors:{
+        primary: "#FFD400",
+        secondary: "#EDF0F5",
+      },
+      fontFamily : {
+        Montserrat: ["Montserrat", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
