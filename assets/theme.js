3c5dc80f60a48db32ab48066f2881be18866b81f
unknown <gokulgandhi97@gmail.com>
2024-02-22 20:15:27 +0530

    t

--- assets/theme.js
+++ assets/theme.js
@@ -2,22 +2,21 @@
   //JS for Back to top btn
 
   const backToTop = document.querySelector("#go-back-top")
-  console.log(backToTop)
-//   const scrollPosition = window.scrollY || document.body.scrollTop;
-  console.log(window.scrollY)
-  backToTop.addEventListener("scroll",function () {
-    if(window.scrollY > 200){
-      backToTop.classList.add("block")
-    } else {
+  
+  window.addEventListener("scroll",function () {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    if(scrollPosition > 200){
       backToTop.classList.remove("hidden")
+    } else {
+      backToTop.classList.add("hidden")
     }
   })
 
   backToTop.addEventListener("click", function () {
-     backToTop.scrollTo({
-    top: 0,
-    behavior: "smooth"
-    }) 
-    // document.body.scrollTop = 0;
-    // document.documentElement.scrollTop = 0
+
+    window.scrollTo({
+        top:0,
+        behavior : "smooth"
+    })
   })
